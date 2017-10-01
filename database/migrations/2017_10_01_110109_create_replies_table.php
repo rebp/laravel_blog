43cc38300daaab1c3386f07d57e464e33a615321
Rober Eduardo Bautista Prieto <r.e.bautista.prieto@hotmail.com>
2017-10-01 13:09:23 +0200

    migrations added

---
+++ database/migrations/2017_10_01_110109_create_replies_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRepliesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('replies', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('comment_id')->unsigned()->index();
+            $table->string('author');
+            $table->string('photo');
+            $table->string('email');
+            $table->text('content');            
+            $table->timestamps();
+
+            $table->foreign('comment_id')->references('id')->on('comments')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('replies');
+    }
+}
